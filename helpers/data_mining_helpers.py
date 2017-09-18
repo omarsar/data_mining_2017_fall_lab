5b19572989495197194d55db00d894393099ffa9
omarsar <ellfae@gmail.com>
2017-09-18 20:00:05 +0800

    added more notes on missing values

--- helpers/data_mining_helpers.py
+++ helpers/data_mining_helpers.py
@@ -21,3 +21,11 @@ def format_rows(docs):
 def format_labels(target, docs):
     """ format the labels """
     return docs.target_names[target]
+
+def check_missing_values(row):
+    """ functions that check and verifies if there are missing values in dataframe """
+    counter = 0
+    for element in row:
+        if element == True:
+            counter+=1
+    return ("The amoung of missing records is: ", counter)
