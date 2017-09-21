de4d17706aaf47b9630d79c5d1714d6f23164d89
omarsar <ellfae@gmail.com>
2017-09-21 15:39:48 +0800

    added preprocessing section

--- helpers/text_analysis.py
+++ helpers/text_analysis.py
@@ -43,6 +43,63 @@ def plot_word_frequency(word_list, plot_title):
     fig = go.Figure(data = data, layout=layout)
     return fig
 
+def plot_heat_map(plot_x, plot_y, plot_z):
+    """ Helper to plot heat map """
+    trace = {
+        "x": plot_x,
+        "y": plot_y,
+        "z": plot_z,
+        "colorscale": [[0.0, "rgb(158,1,66)"], [0.1, "rgb(213,62,79)"], [0.2, "rgb(244,109,67)"], [0.3, "rgb(253,174,97)"], [0.4, "rgb(254,224,139)"], [0.5, "rgb(255,255,191)"], [0.6, "rgb(230,245,152)"], [0.7, "rgb(171,221,164)"], [0.8, "rgb(102,194,165)"], [0.9, "rgb(50,136,189)"], [1.0, "rgb(94,79,162)"]],
+        "type": "heatmap"
+    }
+
+    data = go.Data([trace])
+    layout = {
+        "legend": {
+            "bgcolor": "#F5F6F9",
+            "font": {"color": "#4D5663"}
+        },
+        "paper_bgcolor": "#F5F6F9",
+        "plot_bgcolor": "#F5F6F9",
+        "xaxis1": {
+            "gridcolor": "#E1E5ED",
+            "tickfont": {"color": "#4D5663"},
+            "title": "",
+            "titlefont": {"color": "#4D5663"},
+            "zerolinecolor": "#E1E5ED"
+        },
+        "yaxis1": {
+            "gridcolor": "#E1E5ED",
+            "tickfont": {"color": "#4D5663"},
+            "title": "",
+            "titlefont": {"color": "#4D5663"},
+            "zeroline": False,
+            "zerolinecolor": "#E1E5ED"
+        }
+    }
+
+    fig = go.Figure(data = data, layout=layout)
+    return fig
+
+def get_trace(X_pca, data, category, color):
+    """ Build trace for plotly chart based on category """
+    trace = go.Scatter3d(
+        x=X_pca[data.apply(lambda x: True if x==category else False), 0],
+        y=X_pca[data.apply(lambda x: True if x==category else False),1],
+        z=X_pca[data.apply(lambda x: True if x==category else False),2],
+        mode='markers',
+        marker=dict(
+            size=4,
+            line=dict(
+                color=color,
+                width=0.2
+            ),
+            opacity=0.8
+        ),
+        text=data[data.apply(lambda x: True if x==category else False).tolist()]
+    )
+    return trace
+
 def plot_word_cloud(text):
     """ Generate word cloud given some input text doc """
     word_cloud = WordCloud().generate(text)
